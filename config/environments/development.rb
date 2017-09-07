e01cbd82cc2b96110ce4b40303fdba141290b530
salmaraz741 <salmaraz741@gmail.com>
2017-09-07 07:35:05 +0000

    Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'blogster-steven-almaraz.herokuapp.com' }
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
